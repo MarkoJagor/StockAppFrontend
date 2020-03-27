108dbdceb25a19ac60904adc4d50f4daaff4e5ff
Marko Jagor <majago@ttu.ee>
2020-03-27 13:34:27 +0200

    added filtering by ticker

--- src/components/TickerComponent.js
+++ src/components/TickerComponent.js
@@ -5,6 +5,7 @@ import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import {DataTable} from "primereact/datatable";
 import {Column} from "primereact/column";
+import {InputText} from 'primereact/inputtext';
 
 class TickerComponent extends React.Component {
 
@@ -13,8 +14,6 @@ class TickerComponent extends React.Component {
         this.state = {
             allTickers: []
         };
-
-        this.onClick = this.onClick.bind(this)
     }
 
     componentDidMount() {
@@ -26,11 +25,6 @@ class TickerComponent extends React.Component {
             })
     }
 
-    onClick(rowData) {
-        console.log(rowData.ticker)
-    }
-
-
     render() {
 
         const handleClick = (rowData) => {
@@ -39,21 +33,41 @@ class TickerComponent extends React.Component {
             </div>;
         };
 
+        const tickerFilter = (
+            <div style={{'textAlign': 'left'}}>
+                <i className="pi pi-search" style={{margin: '4px 10px 0 0'}}></i>
+                <InputText type="search" onInput={(e) => this.setState({globalFilter: e.target.value})}
+                           placeholder="Search by Ticker" size="20"/>
+            </div>
+        );
+
         return (
             <div>
-                <DataTable value={this.state.allTickers} autoLayout={true}>
+                <DataTable value={this.state.allTickers} autoLayout={true} header={tickerFilter}
+                           globalFilter={this.state.globalFilter} emptyMessage="No records found">
                     <Column field="ticker" header="Ticker" sortable={true} body={handleClick}/>
-                    <Column field="company" header="Company" sortable={true}/>
-                    <Column field="last_price" header="Last price" sortable={true}/>
-                    <Column field="chg_percentage" header="Chg percentage" sortable={true}/>
-                    <Column field="chg" header="Chg" sortable={true}/>
-                    <Column field="rating" header="Rating" sortable={true}/>
-                    <Column field="volume" header="Volume" sortable={true}/>
-                    <Column field="mkt_cap" header="Market cap" sortable={true}/>
-                    <Column field="p_e" header="P/E" sortable={true}/>
-                    <Column field="eps" header="Eps" sortable={true}/>
-                    <Column field="employees" header="Employees" sortable={true}/>
-                    <Column field="sector" header="Sector" sortable={true}/>
+                    <Column field="company" header="Company" sortable={true}
+                            excludeGlobalFilter={true}/>
+                    <Column field="last_price" header="Last price" sortable={true}
+                            excludeGlobalFilter={true}/>
+                    <Column field="chg_percentage" header="Chg percentage" sortable={true}
+                            excludeGlobalFilter={true}/>
+                    <Column field="chg" header="Chg" sortable={true}
+                            excludeGlobalFilter={true}/>
+                    <Column field="rating" header="Rating" sortable={true}
+                            excludeGlobalFilter={true}/>
+                    <Column field="volume" header="Volume" sortable={true}
+                            excludeGlobalFilter={true}/>
+                    <Column field="mkt_cap" header="Market cap" sortable={true}
+                            excludeGlobalFilter={true}/>
+                    <Column field="p_e" header="P/E" sortable={true}
+                            excludeGlobalFilter={true}/>
+                    <Column field="eps" header="Eps" sortable={true}
+                            excludeGlobalFilter={true}/>
+                    <Column field="employees" header="Employees" sortable={true}
+                            excludeGlobalFilter={true}/>
+                    <Column field="sector" header="Sector" sortable={true}
+                            excludeGlobalFilter={true}/>
                 </DataTable>
             </div>
         )
