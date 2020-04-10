7885f0db829cc35cb77a5bd215ab869454cddedf
Marko Jagor <majago@ttu.ee>
2020-04-10 19:25:56 +0300

    add DividendsTabComponent

---
+++ src/components/DividendsTabComponent.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Table from "./Table";
+
+class DividendsTabComponent extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            dynamicColumns: [
+                {field: "financialsDaily.div_yield", header: "Dividend Yield %", sortable: true, excludeGlobalFilter: true},
+                {field: "financialsQuarterly.div_paid", header: "Dividends Paid", sortable: true, excludeGlobalFilter: true},
+                {field: "financialsQuarterly.div_per_share", header: "Dividend Per Share (FY)", sortable: true, excludeGlobalFilter: true},
+                {field: "financialsQuarterly.shares", header: "Shares", sortable: true, excludeGlobalFilter: true}
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <Table dynamicColumns={this.state.dynamicColumns}
+                   tickerData={this.props.tickerData}
+                   searchByTickerInput={this.props.searchByTickerInput}
+                   updateTickerSearch={this.props.updateTickerSearch}/>
+        )
+    }
+}
+
+export default DividendsTabComponent
