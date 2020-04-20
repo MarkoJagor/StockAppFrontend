9a52ace77945742cf61fd0d017e338e6cd173b59
Marko Jagor <majago@ttu.ee>
2020-04-20 19:47:37 +0300

    converted some values to float

--- src/components/ScreenerDataComponent.js
+++ src/components/ScreenerDataComponent.js
@@ -21,25 +21,25 @@ class ScreenerDataComponent extends React.Component {
                     tickerData: response.data.map((item) => {
                         const financialsDaily = {
                             ...item.financialsDaily,
-                            "div_yield": (item.financialsDaily.div_yield === null) ? null : (item.financialsDaily.div_yield * multiplier).toFixed(2),
-                            "chg": (item.financialsDaily.chg * multiplier).toFixed(2),
-                            "weekly_perf": (item.financialsDaily.weekly_perf * multiplier).toFixed(2),
-                            "monthly_perf": (item.financialsDaily.monthly_perf * multiplier).toFixed(2),
-                            "three_month_perf": (item.financialsDaily.three_month_perf * multiplier).toFixed(2),
-                            "six_month_perf": (item.financialsDaily.six_month_perf * multiplier).toFixed(2),
-                            "ytd_perf": (item.financialsDaily.ytd_perf * multiplier).toFixed(2),
-                            "yearly_perf": (item.financialsDaily.yearly_perf * multiplier).toFixed(2),
-                            "volatility": (item.financialsDaily.volatility * multiplier).toFixed(2)
+                            "div_yield": (item.financialsDaily.div_yield === null) ? null : parseFloat((item.financialsDaily.div_yield * multiplier).toFixed(2)),
+                            "chg": parseFloat((item.financialsDaily.chg * multiplier).toFixed(2)),
+                            "weekly_perf": parseFloat((item.financialsDaily.weekly_perf * multiplier).toFixed(2)),
+                            "monthly_perf": parseFloat((item.financialsDaily.monthly_perf * multiplier).toFixed(2)),
+                            "three_month_perf": parseFloat((item.financialsDaily.three_month_perf * multiplier).toFixed(2)),
+                            "six_month_perf": parseFloat((item.financialsDaily.six_month_perf * multiplier).toFixed(2)),
+                            "ytd_perf": parseFloat((item.financialsDaily.ytd_perf * multiplier).toFixed(2)),
+                            "yearly_perf": parseFloat((item.financialsDaily.yearly_perf * multiplier).toFixed(2)),
+                            "volatility": parseFloat((item.financialsDaily.volatility * multiplier).toFixed(2))
                         };
 
                         const financialsQuarterly = {
                             ...item.financialsQuarterly,
-                            "gross_mrq": (item.financialsQuarterly.gross_mrq * multiplier).toFixed(2),
-                            "operating_mrq": (item.financialsQuarterly.operating_mrq * multiplier).toFixed(2),
-                            "pretax_mrq": (item.financialsQuarterly.pretax_mrq * multiplier).toFixed(2),
-                            "net_mrq": (item.financialsQuarterly.net_mrq * multiplier).toFixed(2),
-                            "roa": (item.financialsQuarterly.roa * multiplier).toFixed(2),
-                            "roe": (item.financialsQuarterly.roe * multiplier).toFixed(2)
+                            "gross_mrq": parseFloat((item.financialsQuarterly.gross_mrq * multiplier).toFixed(2)),
+                            "operating_mrq": parseFloat((item.financialsQuarterly.operating_mrq * multiplier).toFixed(2)),
+                            "pretax_mrq": parseFloat((item.financialsQuarterly.pretax_mrq * multiplier).toFixed(2)),
+                            "net_mrq": parseFloat((item.financialsQuarterly.net_mrq * multiplier).toFixed(2)),
+                            "roa": parseFloat((item.financialsQuarterly.roa * multiplier).toFixed(2)),
+                            "roe": parseFloat((item.financialsQuarterly.roe * multiplier).toFixed(2))
                         };
 
                         return {
