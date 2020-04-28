5ca21d83d624e13d3d068c2d37f0baa673a2e8bf
Marko Jagor <majago@ttu.ee>
2020-04-28 21:37:57 +0300

    added KeyRatiosTabComponent test

---
+++ src/tests/components/TableTabComponents/KeyRatiosTabComponent.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {shallow} from "enzyme";
+import KeyRatiosTabComponent from "../../../components/TableTabComponents/KeyRatiosTabComponent";
+
+describe('KeyRatiosTabComponent', () => {
+    it('passes down correct props to Table component', () => {
+        const wrapper = shallow(<KeyRatiosTabComponent/>)
+        expect(wrapper.find('Table').props()).toMatchObject({
+            dynamicColumns: wrapper.state().dynamicColumns,
+            tickerData: wrapper.props().tickerData,
+            tableData: wrapper.props().tableData,
+            filterInputs: wrapper.props().filterInputs,
+            searchByTickerInput: wrapper.props().searchByTickerInput,
+            updateTickerSearch: wrapper.props().updateTickerSearch,
+            filterTableData: wrapper.props().filterTableData,
+            handleFilterInputChange: wrapper.props().handleFilterInputChange,
+            handleRangeSliderChange: wrapper.props().handleRangeSliderChange,
+            resetFilterInputs: wrapper.props().resetFilterInputs
+        })
+    })
+})
