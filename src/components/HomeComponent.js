dd8dd3b7d8a8ce9656ffac4f42744fab7994555e
Marko Jagor <majago@ttu.ee>
2020-05-02 22:29:33 +0300

    Home Component refactor and styling

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,8 +1,9 @@
 import React from "react";
 import axios from "axios"
 import {Messages} from "primereact/messages";
-import inputStyle from "../componentStyles/HomeComponentStyle.css"
-import StockMarket from "../image/StockMarket.PNG"
+import inputStyle from "../componentStyles/HomeComponentStyle.css";
+import homeComponentStyle from "../componentStyles/HomeComponentStyle.css.js";
+import StockMarket from "../image/StockMarket.PNG";
 import {AutoComplete} from "primereact/autocomplete";
 import {Button} from "primereact/button";
 
@@ -55,70 +56,30 @@ class HomeComponent extends React.Component {
     }
 
     render() {
-        const parentContainer = {
-            position: "relative",
-            height: "100vh",
-        }
-
-        const upperContainer = {
-            width: "100%",
-            height: "50%",
-            textAlign: "center"
-        }
-
-        const lowerContainer = {
-            width: "100%",
-            height: "50%",
-            align: "center"
-        }
-
-        const image = {
-            position: "absolute",
-            width: "100%",
-            height: "50%",
-            opacity: "0.35",
-            zIndex: "-1"
-        }
-
-        const h1 = {
-            margin: "auto",
-            paddingTop: "10vh",
-            width: "100vw",
-            textAlign: "center",
-            fontSize: "8vh"
-        }
-
-        const h2 = {
-            margin: "auto",
-            fontSize: "3.5vh"
-        }
-
-        /*return (
-            <div>
-                <Messages ref={(el) => this.messages = el}/>
-                <div style={inputStyle} className="p-inputgroup">
-                    <AutoComplete value={this.state.ticker} suggestions={this.state.filteredTickers} completeMethod={this.filterTickers}
-                                  size={this.state.tickers.size} placeholder={"Search for tickers"} minLength={1}
-                                  onChange={(e) => this.setState({ticker: e.value})}/>
-                    <Button icon={"pi pi-search"} onClick={this.onSearch}/>
-                </div>
-            </div>
-        )*/
-
         return (
-            <div style={parentContainer}>
-                <img src={StockMarket} style={image} alt={"Stock Market"}/>
-                <div style={upperContainer}>
-                    <h1 style={h1}>Welcome to FinTrust!</h1>
-                    <h2 style={h2}>Your guide to Tallinn Stock Exchange.</h2>
+            <div style={homeComponentStyle.parentContainer}>
+                <img src={StockMarket} style={homeComponentStyle.image} alt={"Stock Market"}/>
+                <div style={homeComponentStyle.upperContainer}>
+                    <h1 style={homeComponentStyle.h1}>
+                        Welcome to FinTrust!
+                    </h1>
+                    <h2 style={homeComponentStyle.h2}>
+                        Your guide to Tallinn Stock Exchange.
+                    </h2>
                 </div>
-                <div style={lowerContainer}>
+                <div style={homeComponentStyle.lowerContainer}>
                     <Messages ref={(el) => this.messages = el}/>
-                    <div style={inputStyle} className="p-inputgroup">
-                        <AutoComplete value={this.state.ticker} suggestions={this.state.filteredTickers} completeMethod={this.filterTickers}
-                                      size={this.state.tickers.size} placeholder={"Search for tickers"} minLength={1}
-                                      onChange={(e) => this.setState({ticker: e.value})}/>
-                        <Button icon={"pi pi-search"} onClick={this.onSearch}/>
+                    <div style={homeComponentStyle.inputGroup}>
+                        <div style={inputStyle} className="p-inputgroup">
+                            <AutoComplete value={this.state.ticker}
+                                          suggestions={this.state.filteredTickers}
+                                          completeMethod={this.filterTickers}
+                                          size={this.state.tickers.size}
+                                          placeholder={"Search for tickers"}
+                                          minLength={1}
+                                          onChange={(e) => this.setState({ticker: e.value})}/>
+                            <Button icon={"pi pi-search"} onClick={this.onSearch}/>
+                        </div>
                     </div>
                 </div>
             </div>
